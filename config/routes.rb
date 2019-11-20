a57d4c708b398f42939ef5e735d6c338ffcf4ca5
Stazia Tronboll <stazia.tronboll@appfolio.com>
2019-11-20 08:41:47 -0800

    Add home controller to serve some dummy content

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
+  root 'home#home'
 end
