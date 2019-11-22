0fdc102fa202819b0a504d331b30498585d68c8a
Stazia Tronboll <stazia.tronboll@appfolio.com>
2019-11-22 15:46:28 -0800

    Add jquery to eslintrc and fix eslint complaints

--- app/javascript/images/index.js
+++ app/javascript/images/index.js
@@ -1,15 +1,12 @@
-let submitBtn = $('.form-actions input[type="submit"]');
-let urlField  = $('#image_url');
-let previewField = $('#image_view .show-img-card');
-let urlRegex = /(https?:\/\/.+)\.(?:jpg|jpeg|gif|png)/g;
+const urlField = $('#image_url');
+const previewField = $('#image_view .show-img-card');
 
 function updateImagePreview() {
   previewField.attr('src', urlField.val());
 }
 
-$(document).ready( () => {
-  updateImagePreview()
+$(document).ready(() => {
+  updateImagePreview();
   $('#image_url').on('change keyup paste', updateImagePreview);
 });
-
 
