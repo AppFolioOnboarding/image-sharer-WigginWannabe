3c103d9ce32e48c2c11591d8e94fff07b4cc4869
Stazia Tronboll <stazia.tronboll@appfolio.com>
2019-11-26 10:30:28 -0800

    ST - Exchange shallow for mount in frontend tests

--- app/javascript/image_sharing/test/components/Form.spec.js
+++ app/javascript/image_sharing/test/components/Form.spec.js
@@ -1,27 +1,27 @@
 /* eslint-env mocha */
 
 import assert from 'assert';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import React from 'react';
 import Form from '../../components/Form';
 
 describe('<Form />', () => {
   it('should have a text input for name', () => {
-    const wrapper = mount(<Form />);
+    const wrapper = shallow(<Form />);
     const nameField = wrapper.find('#feedback_name');
 
     assert(nameField.is('input[type="text"]'));
   });
 
   it('should have a textarea for comments', () => {
-    const wrapper = mount(<Form />);
+    const wrapper = shallow(<Form />);
     const commentField = wrapper.find('#feedback_comments');
 
     assert(commentField.is('textarea'));
   });
 
   it('should have a submit button', () => {
-    const wrapper = mount(<Form />);
+    const wrapper = shallow(<Form />);
     const submitField = wrapper.find('button');
 
     assert(submitField.is('button[type="submit"]'));
