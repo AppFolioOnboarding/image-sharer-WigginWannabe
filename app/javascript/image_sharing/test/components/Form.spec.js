d46c09e32d6ff4ec685cb3b2a10f84fb2d83eb94
Stazia Tronboll <stazia.tronboll@appfolio.com>
2019-11-26 09:39:04 -0800

    ST - Add Form and Footer tests

---
+++ app/javascript/image_sharing/test/components/Form.spec.js
@@ -0,0 +1,30 @@
+/* eslint-env mocha */
+
+import assert from 'assert';
+import { mount } from 'enzyme';
+import React from 'react';
+import Form from '../../components/Form';
+
+describe('<Form />', () => {
+  it('should have a text input for name', () => {
+    const wrapper = mount(<Form />);
+    const nameField = wrapper.find('#feedback_name');
+
+    assert(nameField.is('input[type=text]'));
+  });
+
+  it('should have a textarea for comments', () => {
+    const wrapper = mount(<Form />);
+    const commentField = wrapper.find('#feedback_comments');
+
+    assert(commentField.is('textarea'));
+  });
+
+  it('should have a submit button', () => {
+    const wrapper = mount(<Form />);
+    const submitField = wrapper.find('button');
+
+    assert(submitField.is('button[type=submit]'));
+    assert.strictEqual(submitField.text(), )
+  });
+});
