91b2275bd58a47bf063d298f513fb586bdc28ab6
Stazia Tronboll <stazia.tronboll@appfolio.com>
2019-11-27 10:43:47 -0800

    ST - Create FlashMessage tests

---
+++ app/javascript/image_sharing/test/components/FlashMessage.spec.js
@@ -0,0 +1,45 @@
+/* eslint-env mocha */
+
+import assert from 'assert';
+import { mount } from 'enzyme';
+import React from 'react';
+import FlashMessage from '../../components/FlashMessage';
+
+describe('<FlashMessage />', () => {
+  it('should render with success', () => {
+  	const msg = 'Success message';
+  	const flashMsg = {
+  		type: 'success',
+  		message: msg,
+  	};
+
+    const wrapper = mount(<FlashMessage showFlash={true} flash={flashMsg} />);
+    const alert = wrapper.find('.alert');
+
+    assert.strictEqual(alert.length, 1);
+    assert.strictEqual(alert.text(), msg);
+    assert(alert.hasClass('alert-success'));
+  });
+
+  it('should render with failure', () => {
+  	const msg = 'Failure message';
+  	const flashMsg = {
+  		type: 'danger',
+  		message: msg,
+  	};
+
+    const wrapper = mount(<FlashMessage showFlash={true} flash={flashMsg} />);
+    const alert = wrapper.find('.alert');
+
+    assert.strictEqual(alert.length, 1);
+    assert.strictEqual(alert.text(), msg);
+    assert(alert.hasClass('alert-danger'));
+  });
+
+  it('should not render when showFlash is false', () => {
+  	const wrapper = mount(<FlashMessage showFlash={false} />);
+    const alert = wrapper.find('.alert');
+
+  	assert.strictEqual(alert.length, 0);
+  });
+});
